caaca40ca5bf5300811fb8adc1c45c4d36aca9fc
민수아 <163108073+Minnsuaa@users.noreply.github.com>
2024-08-16 10:56:50 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 176 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+
+        int y = num.nextInt();
+
+        System.out.println(y-543);
+    }
+}
