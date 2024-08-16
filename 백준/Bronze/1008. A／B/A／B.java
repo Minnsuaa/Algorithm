3a78ae91f0ce491866a851c725be2d7b1a9adba6
민수아 <163108073+Minnsuaa@users.noreply.github.com>
2024-08-16 10:41:41 +0900

    [Bronze V] Title: A/B, Time: 168 ms, Memory: 17828 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+
+        int A = num.nextInt();
+        int B = num.nextInt();
+
+        System.out.println((double) A/B);
+    }
+}
