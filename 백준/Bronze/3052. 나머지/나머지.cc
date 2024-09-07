74ef5b95568401bbc2112fc26c456ba2b8e8568e
민수아 <163108073+Minnsuaa@users.noreply.github.com>
2024-09-07 14:07:53 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    int arr[10];
+    int i, j, cnt, result = 0;
+    for(i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+        arr[i] %= 42;
+    }
+    for(i = 0; i < 10; i++) {
+        cnt = 0;
+        for(j = i + 1; j < 10; j++) {
+            if(arr[i] == arr[j])
+                cnt++;
+        }
+        if(cnt == 0)
+            result++;
+    }
+    printf("%d", result);
+}
