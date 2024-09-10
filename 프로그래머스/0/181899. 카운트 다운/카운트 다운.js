ce9276377cd62b6d1f322a68c6c08edb68daa513
민수아 <163108073+Minnsuaa@users.noreply.github.com>
2024-09-10 15:37:05 +0900

    [level 0] Title: 카운트 다운, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -1,8 +1,7 @@
 function solution(start_num, end_num) {
     let answer = [];
-    let count = start_num;
-    for(let i = 0; i <= start_num-end_num; i++) {
-        answer[i] = count--;
+    for(let i = start_num; i >= end_num; i--) {
+        answer.push(i);
     }
     return answer;
 }
