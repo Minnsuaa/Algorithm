bf62828d50db67af83e97782fc72cb50d3e3cef3
민수아 <163108073+Minnsuaa@users.noreply.github.com>
2024-09-17 15:12:59 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.cc
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
@@ -7,8 +8,6 @@ int main()
     scanf("%d", &n);
     for(i = 0; i < n; i++) {
         scanf("%s", arr);
-        for(j=0; arr[j]!='\0'; j++) {
-        }
-        printf("%c%c\n", arr[0], arr[j-1]);
+        printf("%c%c\n", arr[0], arr[strlen(arr)-1]);
     }
 }
