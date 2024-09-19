2ad76e707fcbfe07a8c1e0dd28ebb6327801433d
민수아 <163108073+Minnsuaa@users.noreply.github.com>
2024-09-19 17:07:29 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 4.52 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n, int k) {
+    int answer = 0;
+    answer += n * 12000;
+    if(n >= 10) {
+        k -= n/10;
+    }
+    answer += k * 2000;
+    return answer;
+}
