5a053196c3d5a2cdd449aaec474ba472d084307f
민수아 <163108073+Minnsuaa@users.noreply.github.com>
2024-10-12 21:47:38 +0900

    [level 0] Title: 수 조작하기 1, Time: 5.95 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/README.md
+++ 프로그래머스/0/181926. 수 조작하기 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수 조작하기 1 - 181926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181926?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181926) 
 
 ### 성능 요약
 
-메모리: 90.7 MB, 시간: 47.95 ms
+메모리: 37 MB, 시간: 5.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 25일 21:40:22
+2024년 10월 12일 21:47:35
 
 ### 문제 설명
 
--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,12 +1,12 @@
 function solution(n, control) {
     for(let i = 0; i < control.length; i++) {
-        if(control[i]=="w")
+        if(control[i] == "w")
             n += 1;
-        else if(control[i]=="s")
+        else if(control[i] == "s")
             n -= 1;
-        else if(control[i]=="d")
+        else if(control[i] == "d")
             n += 10;
-        else if(control[i]=="a")
+        else if(control[i] == "a")
             n -= 10;
     }
     return n;
