88cb1b346e22442b1547a7fa0d33b9e2f24feb1a
민수아 <163108073+Minnsuaa@users.noreply.github.com>
2024-10-29 18:46:11 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -1,7 +1,6 @@
 function solution(num_list, n) {
-    let result = [];
-    for(let i = 0; i<n; i++) {
-        result[i] = num_list[i];
-    }
-    return result;
+    let answer = [];
+    for(let i = 0; i < n; i++) 
+        answer.push(num_list[i]);
+    return answer;
 }
