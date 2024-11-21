d6a7a892ddf5ba82e441642ef9b6089fe6e6b162
민수아 <163108073+Minnsuaa@users.noreply.github.com>
2024-11-21 16:50:55 +0900

    [level 0] Title: l로 만들기, Time: 6.46 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String myString) {
+        char[] arr = myString.toCharArray();
+        for(int i = 0; i < arr.length; i++) {
+            if((int)arr[i]<108)
+                arr[i] = 'l';
+        }
+        return new String(arr);
+    }
+}
