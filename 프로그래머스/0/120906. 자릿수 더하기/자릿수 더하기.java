af42337d0b37b61bed46c6ce5a662065c70af414
민수아 <163108073+Minnsuaa@users.noreply.github.com>
2024-12-10 06:26:33 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String a = Integer.toString(n);
+        char[] arr = a.toCharArray();
+        for(int i = 0; i < arr.length; i++) {
+            answer += (int)arr[i]-'0';
+        }
+        return answer;
+    }
+}
